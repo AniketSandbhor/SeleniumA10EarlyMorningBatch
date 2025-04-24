55fc2daaede0bf603faf4ba08ebab108ae24d6cf
Aniket Sandbhor <Aniket Sandbhor@DESKTOP-10J7VNH>
2025-04-24 09:43:25 +0530

    Celebrate the leave

---
+++ src/test/java/Basic/Leave.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Leave {
+	@Test
+	public void leave() {
+		Reporter.log("Celebrate the Leave",true);
+	}
+
+}
